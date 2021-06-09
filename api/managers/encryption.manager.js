2be05f51c774d57d92d8bc491255ae3b3dae9b1a
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2021-06-09 16:12:14 +0530

    addition: Added User Model & Encryption Manager to encrypt sensitive information.

---
+++ api/managers/encryption.manager.js
@@ -0,0 +1,15 @@
+const crypto = require('crypto-js');
+
+function encrypt(data) {
+    return crypto.AES.encrypt(data, process.env.ENCRYPTION_KEY).toString();
+}
+
+function decrypt(data) {
+    const bytes = crypto.AES.decrypt(data, process.env.ENCRYPTION_KEY);
+    return bytes.toString(crypto.enc.Utf8);
+}
+
+module.exports = {
+    encrypt,
+    decrypt
+}
