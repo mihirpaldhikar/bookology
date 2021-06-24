6b4c346bcfdf9fb2a2a1eeeb4e709b4d323b102b
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2021-06-24 18:55:57 +0530

    addition: Added Docker Support for Bookology API.

--- api/middlewares/authorize.middleware.js
+++ api/middlewares/authorize.middleware.js
@@ -1,7 +1,12 @@
 function authorizeToken(request, response, next) {
   const token = request.query.token;
   if (token === 'undefined' || token !== process.env.ACCESS_TOKEN) {
-    response.sendStatus(403);
+    response.status(403).json({
+      result: {
+        message: 'Access token not provided or invalid.',
+        status_code: 403,
+      },
+    });
   } else {
     next();
   }
