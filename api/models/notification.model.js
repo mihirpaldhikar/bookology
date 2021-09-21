3aac7c82a0c03f92de25d83bee1558953ee84322
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2021-09-21 11:12:30 +0530

    api: Added categories field in book.model.js, added seen field in notification.model.js

--- api/models/notification.model.js
+++ api/models/notification.model.js
@@ -2,11 +2,16 @@ const moment = require('moment');
 
 const setNotification = (notification) => {
   return {
-    sender_id: notification.sender_id,
-    receiver_id: notification.receiver_id,
-    notification_title: notification.notification_title,
-    notification_body: notification.notification_body,
-    notification_type: notification.notification_type,
+    metadata: {
+      sender_id: notification.sender_id,
+      receiver_id: notification.receiver_id,
+    },
+    notification: {
+      title: notification.notification_title,
+      body: notification.notification_body,
+      type: notification.notification_type,
+      seen: false,
+    },
     created_on: {
       date: moment().format('dddd DD MMMM YYYY'),
       time: moment().format('hh:mm:ss'),
@@ -16,11 +21,17 @@ const setNotification = (notification) => {
 
 const getNotification = (notification) => {
   return {
-    sender_id: notification.sender_id,
-    receiver_id: notification.receiver_id,
-    notification_title: notification.notification_title,
-    notification_body: notification.notification_body,
-    notification_type: notification.notification_type,
+    notification_id: notification._id,
+    metadata: {
+      sender_id: notification.metadata.sender_id,
+      receiver_id: notification.metadata.receiver_id,
+    },
+    notification: {
+      title: notification.notification.title,
+      body: notification.notification.body,
+      type: notification.notification.type,
+      seen: notification.notification.seen,
+    },
     created_on: {
       date: notification.created_on.date,
       time: notification.created_on.time,
