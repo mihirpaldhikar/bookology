22c419d3ce51a22b1fd02212b5f62f2facc5692b
Mihir Paldhikar <imihirpaldhikar@gmail.com>
2021-10-02 21:35:38 +0530

    api: From now on the Discussion room will be created by the api instead of the app.

--- api/configs/firebase.config.js
+++ api/configs/firebase.config.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const firebaseAdmin = admin.initializeApp({
   credential: admin.credential.applicationDefault(),
 });
-
+admin.firestore().settings({ignoreUndefinedProperties: true});
 module.exports = {
   firebaseAdmin,
 };
